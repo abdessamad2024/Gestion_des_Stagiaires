9e1d4ba782e0a206e55755d594b88cbd5e68686e
abd ellah ikkou <mouniraz772@gmail.com>
2024-11-23 14:26:41 +0100

    abd ellah : fix cennection problem in action

--- fontend/Home/Action.php
+++ fontend/Home/Action.php
@@ -6,7 +6,7 @@ $search = isset($_POST['search']) ? $_POST['search'] : null;
 
 if ($search !== null) {
     $sql = "SELECT * FROM stagiaires WHERE matStagiaire = :matricule";
-    $stmt = $pdo->prepare($sql);
+    $stmt = $connection->prepare($sql);
     $stmt->execute([':matricule' => $search]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 } else {
